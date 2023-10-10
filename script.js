a6366a4e0265ef7c4b1af4bb6ffa7bafcab07fc6
kingWahley <kingwahley2@gmail.com>
2023-10-10 19:52:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+// function toggleMenu() {
+//   const menu = document.querySelector(".menu-links");
+//   const icon = document.querySelector(".hamburger-icon");
+//   menu.classList.toggle("open");
+//   icon.classList.toggle("open");
+// }
+
+// const SkillSection = document.getElementById("skillSection");
+
+// const skills = [
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=HTML5&color=E34F26&logo=HTML5&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=CSS&color=1572B6&logo=CSS3&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Tailwind+CSS&color=38B2AC&logo=Tailwind+CSS&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=JavaScript&color=F7DF1E&logo=JavaScript&logoColor=000000&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Sass&color=CC6699&logo=Sass&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=React&color=61DAFB&logo=React&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Next.js&color=000000&logo=Next.js&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Dart&color=0175C2&logo=Dart&logoColor=FFFFFF&label=Intermediate",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Egitlementor&color=00A98F&logo=Elementor&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Firebase&color=FFCA28&logo=Firebase&logoColor=FFFFFF&label=Intermediate",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Netlify&color=00C7B7&logo=Netlify&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Visual+Studio&color=5C2D91&logo=Visual+Studio&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Git&color=F05032&logo=Git&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=GitHub&color=181717&logo=GitHub&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Bootstrap&color=7952B3&logo=Bootstrap&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=TypeScript&color=3178C6&logo=TypeScript&logoColor=FFFFFF&label=Intermediate",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Python&color=3776AB&logo=Python&logoColor=FFFFFF&label=Intermediate",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=WordPress&color=21759B&logo=WordPress&logoColor=FFFFFF&label=Experienced",
+
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Figma&color=F24E1E&logo=Figma&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Adobe+XD&color=FF61F6&logo=Adobe+XD&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Photoshop&color=31A8FF&logo=Adobe+Photoshop&logoColor=FFFFFF&label=Experienced",
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Adobe+Illustrator&color=FF9A00&logo=Adobe+Illustrator&logoColor=FFFFFF&label=Intermediate",
+
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Linux&color=FCC624&logo=Linux&logoColor=000000&label=Experienced",
+
+//   "https://img.shields.io/static/v1?style=for-the-badge&message=Node.js&color=339933&logo=Node.js&logoColor=FFFFFF&label=Intermediate",
+// ];
+
+// function generateSkillHTML(skill) {
+//   return `<div class="skillSet"><img class="skills" src="${skill}" alt=""></div> `;
+// }
+
+// const html = skills.map(generateSkillHTML).join("");
+
+// SkillSection.insertAdjacentHTML("beforeend", html);
